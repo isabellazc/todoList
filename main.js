d5ee002496c845a01b3bd29813c0f37e5f0a6708
Alexa Zam <alexazch@gmail.com>
2022-12-11 20:40:03 -0500

    updates

--- main.js
+++ main.js
@@ -15,8 +15,9 @@ function addTodo(action){
     renderTodo(item)
 }
 
-const input = document.getElementById('new-item');
 //Add new item to list 
+const input = document.getElementById('new-item');
+
 input.addEventListener('keypress', (event) => {
     if(event.key === "Enter"){
         if(input.value !== ''){
@@ -30,14 +31,7 @@ input.addEventListener('keypress', (event) => {
 
 })
 
-//display options for every todo
-/*const moreMenu = document.getElementsByClassName('dropbtn');
-for(let i = 0; i < moreMenu.length; i++){
-    moreMenu[i].addEventListener('click', ()=>{
-        let content = moreMenu[i].getElementsByClassName('dropdown-content');
-        content[0].classList.remove('hidden')
-    })
-}*/
+
 
 
 //Add new title to the list
@@ -57,13 +51,33 @@ title.addEventListener('keypress', (event) =>{
 let ul = document.getElementById('list');
 ul.addEventListener('click' ,(event)=>{
     if(event.target.style.textDecoration != 'line-through'){
-        event.target.style.textDecoration = 'line-through'
+       
+        event.target.style.textDecoration = 'line-through' //fix this: it's also putting a line on the x
+        list.forEach(element => {
+            if(element.action === event.target.innerHTML){
+                element.completed = true
+            }
+            
+        })
     }else{
         event.target.style.textDecoration = 'none'
+        list.forEach(element => {
+            if(element.action === event.target.innerHTML){
+                element.completed = false
+            }
+            
+        })
     }
     
 })
-
+//delete todo
+ul.addEventListener('click', (event)=>{
+    if (event.target.classList.contains('deleteBttn')){
+       //delete item from the list 
+       //delete li from ul
+    }
+    return
+})
 
 
 //Render list
@@ -74,24 +88,17 @@ function renderTodo(listTodo){
     liElement.innerHTML =` 
     <div  id="${listTodo.id}" class="list-item">
     <p>${listTodo.action}</p>
-    <i class="dropbtn fa-solid fa-ellipsis-vertical">
-    <div class=" dropdown-content">
-    <a href="#">Delete</a>
-    <a href="#">Edit</a>
     </div></i>
     
     </div>
    `
         
     myList.appendChild(liElement)
-    console.log(list)
 
 }
 
 
 
-//crossline items when click
-//settings button in the top left
 //night mode
-//activate dots buttons
+
 
